48306a5fa2a672f337974bd1cb9d84709f8ef772
Jakub Kulich <jakubkulich@gmail.com>
2016-11-20 23:05:54 +0100

    chore(css): CSS font imports moved from html to App.vue so webpack can pack them

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,7 @@
 <style>
   @import url(https://fonts.googleapis.com/css?family=Lato:300);
+  @import url(https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic);
+  @import url(https://fonts.googleapis.com/icon?family=Material+Icons);
 
   * {
     margin: 0;
@@ -19,7 +21,7 @@
       );
     background-position: center;
     display: flex;
-    font-family: Lato, Helvetica, sans-serif;
+    font-family: Roboto, Helvetica, sans-serif;
     justify-content: center;
     text-align: center;
   }
