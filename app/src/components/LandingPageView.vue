875e6ced6401bb4c06819414be5d19adff0ff9ad
Jakub Kulich <jakubkulich@gmail.com>
2016-11-20 22:40:55 +0100

    Repo init

---
+++ app/src/components/LandingPageView.vue
@@ -0,0 +1,66 @@
+<style scoped>
+  img {
+    margin-top: 25px;
+    width: 450px;
+  }
+</style>
+
+<template>
+  <div>
+    <md-toolbar class="md-medium">
+      <div class="md-toolbar-container">
+        <md-button class="md-icon-button">
+          <md-icon>menu</md-icon>
+        </md-button>
+
+        <h2 class="md-title" style="flex: 1;">Vue Material</h2>
+
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+      </div>
+    </md-toolbar>
+    <img src="./LandingPageView/assets/logo.png" alt="electron-vue">
+    <h1>Welcome.</h1>
+    <current-page></current-page>
+    <versions></versions>
+    <links></links>
+    <md-tabs md-fixed>
+      <md-tab md-label="Movies" md-icon="ondemand_video">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      </md-tab>
+
+      <md-tab md-label="Music" md-icon="music_note">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      </md-tab>
+
+      <md-tab md-label="Books" md-icon="books">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+      </md-tab>
+
+      <md-tab md-label="Pictures" md-icon="photo">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+      </md-tab>
+    </md-tabs>
+  </div>
+</template>
+
+<script>
+  import CurrentPage from './LandingPageView/CurrentPage'
+  import Links from './LandingPageView/Links'
+  import Versions from './LandingPageView/Versions'
+
+  export default {
+    components: {
+      CurrentPage,
+      Links,
+      Versions
+    },
+    name: 'landing-page'
+  }
+</script>
