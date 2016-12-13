08d15b7d8cd90c4c0405e97cf96873d1db428acf
smiley <adrian5ster@gmail.com>
2016-12-13 18:08:40 +0100

    fix: Tweaked album card

--- app/src/components/LandingPageView/Album.vue
+++ app/src/components/LandingPageView/Album.vue
@@ -6,11 +6,25 @@
     .md-card .md-card-header {
         padding: 8px;
     }
+    .album-card:hover .album-fade {
+        /*opacity: 1;*/
+    }
+    .album-fade {
+        /*opacity: 0;
+        transition: opacity 0.2s ease;
+        will-change: transition;*/
+    }
+    .album-fade .md-title {
+        font-size: 16px;
+    }
+    .album-fade .md-subhead {
+        font-size: 12px;
+    }
 </style>
 <template>
     <div>
         <router-link :to="{name: 'album', params: { id: album.id }}">
-        <md-card>
+        <md-card class="album-card">
             <md-ink-ripple></md-ink-ripple>
             <md-card-media-cover md-solid>
                 <md-card-media md-ratio="1:1">
@@ -18,7 +32,7 @@
                     <img v-if="album.thumbnail === null" src="./assets/noart.png" alt="Skyscraper">
                 </md-card-media>
 
-                <md-card-area>
+                <md-card-area class="album-fade">
                     <md-card-header>
                         <div class="md-title">{{album.name}}</div>
                         <div class="md-subhead">{{album.artist}}</div>
